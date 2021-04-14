2579ae3103d44b9ce785525ae73524b34cdd78a6
ty要nx <ttyy373172968@163.com>
2021-04-14 16:15:13 +0800

    初始化四个页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login.vue'
+import NotebookList from '@/components/NotebookList.vue'
+import NoteDetail from '@/components/NoteDetail.vue'
+import TrashDetail from '@/components/TrashDetail.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/notebooks',
+      name: 'NotebookList',
+      component: NotebookList
+    },
+    {
+      path: '/note/:noteId',
+      name: 'NoteDetail',
+      component: NoteDetail
+    }
+    ,
+    {
+      path: '/trash/:noteId',
+      name: 'TrashDetail',
+      component: TrashDetail
     }
   ]
 })
