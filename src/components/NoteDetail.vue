6ed1add35188de4e87c33b81490f125773da15ad
ttyy <ttyy373172968@163.com>
2021-04-19 13:06:58 +0800

    实现笔记本详情页基本页面

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -1,14 +1,21 @@
 <template>
-  <div id="note-detail">
-    <h1>notebookId : {{ $route.query.notebookId }}</h1>
-    <h1>noteId : {{ $route.query.noteId }}</h1>
-  </div>
+    <div id="note" class="detail">
+      <NoteSideBar/>
+      <div id="note-detail">
+        <h1>notebookId : {{ $route.query.notebookId }}</h1>
+        <h1>noteId : {{ $route.query.noteId }}</h1>
+      </div>
+    </div>
 </template>
 
 <script>
 import Auth from '@/apis/auth'
+import NoteSideBar from '@/components/NoteSideBar.vue'
 
 export default {
+  components:{
+    NoteSideBar
+  },
   data() {
     return {
       msg: '笔记本详情列表'
@@ -24,8 +31,13 @@ export default {
 }
 </script>
 
-<style scoped>
-h1 {
-  color: blueviolet;
+<style lang="less" scoped>
+@import url(../assets/css/note-detail.less);
+
+#note {
+  display: flex;
+  align-items: stretch;
+  background-color: #fff;
+  flex: 1;
 }
 </style>
