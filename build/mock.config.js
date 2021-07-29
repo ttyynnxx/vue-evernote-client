eb6c2d967874a97e0afb02d6e310d91bb3297b76
ty要nx <ttyy373172968@163.com>
2021-07-29 20:52:25 +0800

    修复打包后图片不显示问题

--- build/mock.config.js
+++ build/mock.config.js
@@ -2,13 +2,16 @@ const fs = require('fs')
 const path = require('path')
 
 const mockBaseURL = '//note-server.hunger-valley.com'
-const realBaseURL = 'https://note-server.hunger-valley.com'
+const realBaseURL = '//note-server.hunger-valley.com'
 
-exports.config = function({ isDev = true } = { isDev: true} ) {
+exports.config = function({ isDev = true } = { isDev: true }) {
   let fileTxt = `
   module.exports = {
     baseURL: '${isDev ? mockBaseURL : realBaseURL}'
   }
   `
-  fs.writeFileSync(path.join(__dirname, '../src/helpers/config-baseURL.js'), fileTxt)
+  fs.writeFileSync(
+    path.join(__dirname, '../src/helpers/config-baseURL.js'),
+    fileTxt
+  )
 }
